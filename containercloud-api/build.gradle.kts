1eecafd1586322ceec1aeee854a4298bcf5ce99a
Anthony T <ungegrieftfirma@gmail.com>
2023-09-17 14:28:51 +0200

    build(update gradle build): add lombok

--- containercloud-api/build.gradle.kts
+++ containercloud-api/build.gradle.kts
@@ -10,10 +10,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
 }
