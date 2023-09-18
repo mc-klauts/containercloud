fb0208b827f25a6b1d2b6e34a64accf8ad58d32a
Anthony T <ungegrieftfirma@gmail.com>
2023-09-18 20:35:19 +0200

    style(removed imports)

--- containercloud-api/src/main/java/de/containercloud/api/task/Task.java
+++ containercloud-api/src/main/java/de/containercloud/api/task/Task.java
@@ -1,12 +1,10 @@
 package de.containercloud.api.task;
 
 import de.containercloud.api.ServiceType;
-import de.containercloud.api.service.Service;
 import de.containercloud.api.service.configuration.ServiceConfiguration;
 import de.containercloud.api.template.Template;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface Task {
 
