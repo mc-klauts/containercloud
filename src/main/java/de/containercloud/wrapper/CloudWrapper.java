50eb4d86d08310650add26042bfb58af71876eb8
Anthony T <shortexceptionde@gmail.com>
2023-09-23 12:49:40 +0200

    style(added logging)

--- src/main/java/de/containercloud/wrapper/CloudWrapper.java
+++ src/main/java/de/containercloud/wrapper/CloudWrapper.java
@@ -12,6 +12,7 @@ import de.containercloud.impl.service.ServiceManagerImpl;
 import de.containercloud.registry.CloudRegistryImpl;
 import de.containercloud.web.CloudSocketServer;
 import lombok.Getter;
+import lombok.val;
 import org.slf4j.Logger;
 
 @Getter
@@ -35,8 +36,14 @@ public class CloudWrapper {
 
         logger.info("Starting Cloud Wrapper...");
 
+        // Start all sub wrapper
         this.containerWrapper = new CloudContainerWrapper(this.dockerClient, this.databaseHandler);
+
+        logger.info("Starting Sockets...");
         this.socketServer = new CloudSocketServer();
+
+        // api management
+        logger.info("Starting API Services...");
 
         this.serviceManager = new ServiceManagerImpl();
 
@@ -45,6 +52,12 @@ public class CloudWrapper {
         this.eventManager = new EventManagerImpl();
 
         registry.addService(EventManager.class, eventManager);
+
+        logger.info("CloudWrapper start finished!");
+
+        val cloudPullImages = new CloudPullImages(this.dockerClient);
+
+        cloudPullImages.pullImage("itzg/minecraft-server:latest");
     }
 
 
