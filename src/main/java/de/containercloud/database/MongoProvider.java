8c4e764ff494636af6fbc527d9c8a2b154ccd239
Anthony T <shortexceptionde@gmail.com>
2023-09-23 12:50:28 +0200

    style(added method without lombok)

--- src/main/java/de/containercloud/database/MongoProvider.java
+++ src/main/java/de/containercloud/database/MongoProvider.java
@@ -8,19 +8,21 @@ import lombok.Getter;
 @Getter
 public class MongoProvider {
 
-    @Getter
     private static MongoProvider INSTANCE;
-
     private final MongoServiceHandler serviceHandler;
     private final MongoTaskHandler taskHandler;
     private final MongoTemplateHandler templateHandler;
-
     public MongoProvider(MongoDatabaseHandler databaseHandler) {
         INSTANCE = this;
 
         this.serviceHandler = new MongoServiceHandler(databaseHandler);
         this.taskHandler = new MongoTaskHandler(databaseHandler);
         this.templateHandler = new MongoTemplateHandler(databaseHandler);
+    }
+
+    @SuppressWarnings("LombokGetterMayBeUsed")
+    public static MongoProvider getINSTANCE() {
+        return INSTANCE;
     }
 
 }
