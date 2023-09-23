074adc4d4cbb0fd26c79dcfc7ace425cb2153587
Anthony T <shortexceptionde@gmail.com>
2023-09-23 12:47:41 +0200

    feature(start container on create)

--- src/main/java/de/containercloud/wrapper/CloudContainerWrapper.java
+++ src/main/java/de/containercloud/wrapper/CloudContainerWrapper.java
@@ -9,37 +9,66 @@ import de.containercloud.api.service.Service;
 import de.containercloud.api.task.Task;
 import de.containercloud.database.CloudMongoCollection;
 import de.containercloud.database.MongoDatabaseHandler;
+import de.containercloud.database.MongoProvider;
 import de.containercloud.env.EnvConfig;
 import de.containercloud.impl.service.ServiceImpl;
-import lombok.RequiredArgsConstructor;
+import de.containercloud.impl.task.TaskImpl;
+import de.containercloud.web.socket.services.ListServices;
 import lombok.val;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
 public class CloudContainerWrapper {
 
     private final DockerClient dockerClient;
     private final Map<String, Service> runningContainers = new HashMap<>();
     private final MongoDatabaseHandler databaseHandler;
 
+    public CloudContainerWrapper(DockerClient dockerClient, MongoDatabaseHandler databaseHandler) {
+        this.dockerClient = dockerClient;
+        this.databaseHandler = databaseHandler;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+
+            runningContainers.keySet().forEach(s -> {
+
+                val taskId = runningContainers.get(s).task().taskId();
+
+                val task = MongoProvider.getINSTANCE().getTaskHandler().task(taskId);
+
+                task.runningServices().remove(s);
+
+                MongoProvider.getINSTANCE().getTaskHandler().updateTask(task);
+
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    this.dockerClient.stopContainerCmd(s).exec();
+                    this.dockerClient.removeContainerCmd(s).exec();
+                }));
+
+            });
+
+            runningContainers.clear();
+        }));
+    }
+
     public List<String> listRunningContainers() {
         return new ArrayList<>(runningContainers.keySet());
     }
 
-    private String createServiceName(Task task) {
+    private @NotNull String createServiceName(Task task) {
 
         val collection = EnvConfig.getCollectionEnv(CloudMongoCollection.CollectionTypes.SERVICE);
 
         this.databaseHandler.collection(collection).find(Filters.eq(""));
 
-        return "";
+        return task.taskId() + "-" + task.runningServices().size();
     }
 
-    public ServiceImpl runService(Task task) {
+    public ServiceImpl runService(TaskImpl task) {
 
         val serviceName = createServiceName(task);
 
@@ -49,16 +78,25 @@ public class CloudContainerWrapper {
                         "EULA=true");
 
         createContainer
-                .getHostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPortRange(50000, 60000), ExposedPort.tcp(25565)));
+                .getHostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPortRange(task.configuration().runningPort().from(), task.configuration().runningPort().to()), ExposedPort.tcp(25565)));
 
         val response = createContainer.withVolumes()
                 .exec();
 
+
         val id = response.getId();
+
+        task.runningServices().add(id);
+
+        MongoProvider.getINSTANCE().getTaskHandler().updateTask(task);
+
+        this.dockerClient.startContainerCmd(id).exec();
 
         val service = new ServiceImpl(id, task.taskId(), serviceName);
 
         this.runningContainers.put(id, service);
+
+        ListServices.broadcastServiceUpdate(this.runningContainers.values().stream().toList());
 
         return service;
     }
