d01bf100e194858bbf44691e41f0cd6e7bac7b2d
DasShorty <shortexceptionde@gmail.com>
2023-09-25 15:20:49 +0200

    style(SPACE): added space and empty lines

--- src/main/java/de/containercloud/web/CloudSocketServer.java
+++ src/main/java/de/containercloud/web/CloudSocketServer.java
@@ -3,6 +3,7 @@ package de.containercloud.web;
 import de.containercloud.web.http.services.CreateServiceRequest;
 import de.containercloud.web.http.tasks.CreateTaskRequest;
 import de.containercloud.web.socket.services.ListServices;
+import de.containercloud.web.socket.tasks.ListTasks;
 import io.javalin.Javalin;
 
 public class CloudSocketServer {
@@ -10,10 +11,16 @@ public class CloudSocketServer {
     private final Javalin javalin;
 
     public CloudSocketServer() {
+
         this.javalin = Javalin.create()
                 .start(50000);
 
+        // Websockets
         this.javalin.ws("/services/list", ListServices::new);
+        this.javalin.ws("/tasks/list", ListTasks::new);
+
+
+        // Http
         this.javalin.post("/services/create", new CreateServiceRequest());
         this.javalin.post("/tasks/create", new CreateTaskRequest());
 
