04beec96bb426ccaafe3593d96f746a2a72bc3ab
DasShorty <shortexceptionde@gmail.com>
2023-09-30 15:23:42 +0200

    style(removed imports): unused imports has been removed

--- containercloud-api/src/main/java/de/containercloud/api/task/Task.java
+++ containercloud-api/src/main/java/de/containercloud/api/task/Task.java
@@ -1,10 +1,7 @@
 package de.containercloud.api.task;
 
-import de.containercloud.api.ServiceType;
 import de.containercloud.api.service.configuration.ServiceConfiguration;
 import de.containercloud.api.template.Template;
-
-import java.util.List;
 
 public interface Task {
 
@@ -18,7 +15,7 @@ public interface Task {
     /**
      * @param template the new template for the task
      * @return if the template update was successfully
-     * */
+     */
     boolean template(Template template);
 
     String taskId();
